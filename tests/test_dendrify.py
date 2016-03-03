aa966e306490c381ea4df597866d9aa51216eadb
Ben North <ben@redfrontdoor.org>
2016-03-03 19:34:53 +0000

    Add test skeleton with empty_repo fixture

---
+++ tests/test_dendrify.py
@@ -0,0 +1,12 @@
+import pytest
+import pygit2 as git
+
+
+@pytest.fixture
+def empty_repo(tmpdir):
+    return git.init_repository(tmpdir.strpath)
+
+
+def test_empty_repo(empty_repo):
+    all_refs = empty_repo.listall_references()
+    assert len(all_refs) == 0
