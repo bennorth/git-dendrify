d4000ae924d259a916a250af2c486316de1d5e2a
Ben North <ben@redfrontdoor.org>
2016-03-03 19:48:43 +0000

    empty_dendrifier: Add fixture

--- tests/test_dendrify.py
+++ tests/test_dendrify.py
@@ -14,6 +14,11 @@ def empty_repo(tmpdir):
     return git.init_repository(tmpdir.strpath)
 
 
+@pytest.fixture
+def empty_dendrifier(empty_repo):
+    return dendrify.Dendrifier(empty_repo.path)
+
+
 def test_empty_repo(empty_repo):
     all_refs = empty_repo.listall_references()
     assert len(all_refs) == 0
