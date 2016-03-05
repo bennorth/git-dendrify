8e7245e61fd53da2814b6c3d19a7d03757abf67d
Ben North <ben@redfrontdoor.org>
2016-03-05 20:08:28 +0000

    populate_repo(): Add
    Light interactive testing.

--- tests/test_dendrify.py
+++ tests/test_dendrify.py
@@ -1,5 +1,6 @@
 import pytest
 import pygit2 as git
+import time
 
 try:
     import dendrify
@@ -23,6 +24,31 @@ def test_empty_repo(empty_repo):
     all_refs = empty_repo.listall_references()
     assert len(all_refs) == 0
 
+
+def populate_repo(repo, commit_descriptors):
+    assert not dendrify.repo_has_branch(repo, 'linear')
+    linear_branch = dendrify.create_base(repo, 'linear')
+
+    # TODO: Extract from config.
+    sig = git.Signature('Nobody', 'nobody@example.com', time=int(time.time()))
+
+    for idx, cd in enumerate(commit_descriptors):
+        parent = repo[repo.lookup_branch('linear').target]
+        def commit(msg, tree_oid):
+            repo.create_commit('refs/heads/linear', sig, sig,
+                               msg, tree_oid, [parent.oid.hex])
+        if cd == '[':
+            commit('<s>Start work {}'.format(idx), parent.tree.oid)
+        elif cd == ']':
+            commit('</s>Finish work {}'.format(idx), parent.tree.oid)
+        elif cd == '.':
+            blob = repo.create_blob('{}\n'.format(idx).encode('utf-8'))
+            tb = repo.TreeBuilder(parent.tree)
+            tb.insert('data', blob, git.GIT_FILEMODE_BLOB)
+            tree_oid = tb.write()
+            commit('Work item {}'.format(idx), tree_oid)
+        else:
+            raise ValueError('unknown commit-descriptor')
 
 class TestTransformations:
     def test_ensure_base(self, empty_dendrifier):
