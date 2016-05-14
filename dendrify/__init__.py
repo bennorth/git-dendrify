0b2ebaadbcf18f293a85bc4286a4d482d27cd49b
Ben North <ben@redfrontdoor.org>
2016-05-14 14:19:51 +0100

    plain_message_from_tagged(): Handle end/start

--- dendrify/__init__.py
+++ dendrify/__init__.py
@@ -65,6 +65,8 @@ class Dendrifier:
         if msg.startswith('<s>'):
             return msg[3:]
         if msg.startswith('</s>'):
+            if msg.endswith('<s>'):
+                return msg[4:-3]
             return msg[4:]
         return msg
 
