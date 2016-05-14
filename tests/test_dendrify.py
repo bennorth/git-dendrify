a9750184d63331e7c3ad6013114fe32efdd48afd
Ben North <ben@redfrontdoor.org>
2016-05-14 14:42:48 +0100

    test_dendrify(): Add more linear commits
    Preparation for turning one of them into an end/start.

--- tests/test_dendrify.py
+++ tests/test_dendrify.py
@@ -145,14 +145,16 @@ class TestTransformations:
                              '[', # 1
                              '[', # 2
                              '.', # 3
-                             ']', # 4
-                             ']', # 5
-                             '.', # 6
+                             '.', # 4
+                             '.', # 5
+                             ']', # 6
+                             ']', # 7
+                             '.', # 8
                              ])
         empty_dendrifier.dendrify('dendrified', 'develop', 'linear')
         lin_commit_oids = empty_dendrifier.linear_ancestry('develop', 'linear')
-        exp_links = [(6, 5), (5, 4), (5, 1), (4, 3), (4, 2),
-                     (3, 2), (2, 1), (1, 0), (0, -1)]
+        exp_links = [(8, 7), (7, 6), (7, 1), (6, 5), (6, 2),
+                     (5, 4), (4, 3), (3, 2), (2, 1), (1, 0), (0, -1)]
 
         develop_oid = repo.lookup_branch('develop').target
         dendrified_tip_oid = repo.lookup_branch('dendrified').target
