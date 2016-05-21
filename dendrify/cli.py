9999b2e3d6df18dd2face800e9e104d7b1d61da1
Ben North <ben@redfrontdoor.org>
2016-05-21 23:13:27 +0100

    Skeleton main() for CLI using docopt

--- dendrify/cli.py
+++ dendrify/cli.py
@@ -1,5 +1,16 @@
+"""git-dendrify
+
+Usage:
+  git-dendrify (-h | --help)
+  git-dendrify --version
+  git-dendrify dendrify <new-branch> <base-commit> <linear-commit>
+  git-dendrify linearize <new-branch> <base-commit> <dendrified-commit>
+"""
+
 import pygit2 as git
 import dendrify
+import docopt
+from dendrify._version import __version__
 
 def dendrifier_for_path(dirname, _ceiling_dir_for_testing=''):
     try:
@@ -7,3 +18,6 @@ def dendrifier_for_path(dirname, _ceiling_dir_for_testing=''):
     except KeyError:
         raise ValueError('could not find git repo starting from {}'.format(dirname))
     return dendrify.Dendrifier(repo)
+
+def main():
+    args = docopt.docopt(__doc__, version='git-dendrify {}'.format(__version__))
