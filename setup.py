b15e0c4372074bbdd827ba84f63f126fff77a141
Ben North <ben@redfrontdoor.org>
2016-05-21 22:56:46 +0100

    Read version for setup from _version file

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import re
 from setuptools import setup, find_packages
+
+with open('dendrify/_version.py') as f_in:
+    m = re.match("__version__ = '([^']*)'", f_in.read())
+    _version = m.group(1)
 
 setup(
     name = 'gitdendrify',
-    version = '0.1.0',
+    version = _version,
     packages = find_packages(),
 )
