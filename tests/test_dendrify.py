2211cef13c2e522ae25c792c3e37395f1c010787
Ben North <ben@redfrontdoor.org>
2016-05-21 08:28:40 +0100

    test_dendrify(): Remove EndStart commits
    Update expected links; but test fails because those links are not yet
    created.  Will fix shortly.

--- tests/test_dendrify.py
+++ tests/test_dendrify.py
@@ -138,16 +138,17 @@ class TestTransformations:
                              '[', # 1
                              '[', # 2
                              '.', # 3
-                             '|', # 4
-                             '.', # 5
-                             ']', # 6
+                             ']', # 4
+                             '[', # 5
+                             '.', # 6
                              ']', # 7
-                             '.', # 8
+                             ']', # 8
+                             '.', # 9
                              ])
         empty_dendrifier.dendrify('dendrified', 'develop', 'linear')
         lin_commit_oids = empty_dendrifier.linear_ancestry('develop', 'linear')
-        exp_links = [(8, 7), (7, 6), (7, 1), (6, 5), (6, 4), (4, 2),
-                     (5, 4), (4, 3), (3, 2), (2, 1), (1, 0), (0, -1)]
+        exp_links = [(9, 8), (8, 7), (8, 0), (7, 6), (7, 4), (6, 5),
+                     (5, 4), (4, 3), (4, 1), (3, 2), (2, 1), (1, 0), (0, -1)]
 
         develop_oid = repo.lookup_branch('develop').target
         dendrified_tip_oid = repo.lookup_branch('dendrified').target
